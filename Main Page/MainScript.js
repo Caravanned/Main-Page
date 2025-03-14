aa6428f42fc1034aaa5f934e1cbc872105d6990c
Caravanned <vandanganan@gmail.com>
2025-03-14 20:22:46 +0800

    Add files via upload

--- Main Page/MainScript.js
+++ Main Page/MainScript.js
@@ -1,6 +1,8 @@
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
+const menu = document.querySelector('.hidden-nav-list'); // Get the menu element
+const clicked = document.querySelector('.hide-on-desktop'); // Get the mobile menu icon
 
 productContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
@@ -13,31 +15,33 @@ productContainers.forEach((item, i) => {
     preBtn[i].addEventListener('click', () => {
         item.scrollLeft -= containerWidth;
     })
-})
+});
 
 function showMenu() {
-    const menu = document.querySelector('.hidden-nav-list');
-    const clicked = document.querySelector('.hide-on-desktop');
-    clicked.addEventListener('click', hideMenu);
+    menu.classList.add('active');
     menu.style.display = 'flex';
+    clicked.onclick = hideMenu;
 }
 
 function hideMenu() {
-    const menu = document.querySelector('.hidden-nav-list');
-    const clicked = document.querySelector('.hide-on-desktop');
-    clicked.addEventListener('click', showMenu);
+    menu.classList.remove('active');
     menu.style.display = 'none';
+    clicked.onclick = showMenu;
 }
 
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    const navList = document.querySelector('.nav-list'); 
+    const header = document.querySelector('.header-nobootstrap');
+    const navList = document.querySelector('.nav-list');
 
     if (window.scrollY > 0) {
-        header.classList.add('scrolled'); 
-        navList.style.display = 'none'; 
+        header.classList.add('scrolled');
+        navList.style.display = 'none';
+        if(menu.classList.contains('active')){ // if the menu is active while scrolling, hide it
+            hideMenu();
+        }
     } else {
-        header.classList.remove('scrolled'); 
-        navList.style.display = ''; 
+        header.classList.remove('scrolled');
+        navList.style.display = '';
     }
 });
+
